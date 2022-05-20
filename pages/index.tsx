438c6d91617ecc9cc7f03fba37234f5922bded2a
Devesh Goyal <Devesh26goyal@gmail.com>
2022-05-20 23:27:49 +0530

    Add

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Home = ({
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
-        <title>Create Next App</title>
+        <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
